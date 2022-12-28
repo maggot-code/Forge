f2d2d97f989f6af925d42b130be8986aaa65bd89
maggot_code <wowhuanhuan233@163.com>
2022-12-28 18:16:22 +0800

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,17 +3,22 @@
  * @Author: maggot-code
  * @Date: 2022-12-23 15:51:23
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-12-27 18:07:06
+ * @LastEditTime: 2022-12-28 18:14:31
  * @Description: 
  */
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-    entry: ["./packages/**/*.ts"],
-    format: ["esm", "cjs"],
-    dts: true,
-    splitting: true,
-    sourcemap: true,
-    clean: true,
-    treeshake: true
+export default defineConfig((options) => {
+    console.log(options);
+
+    return {
+        entry: ["./packages/**/*.ts"],
+        format: ["esm", "cjs"],
+        dts: true,
+        splitting: true,
+        sourcemap: true,
+        clean: true,
+        treeshake: true,
+        minify: !options.watch,
+    };
 });
